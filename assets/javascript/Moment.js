316496c25031af8527698f56de05ee9a1c6a8240
Eliska Patockova <patockovaeliska@gmail.com>
2020-03-25 11:37:33 -0700

    added a function to write a new train to the schedule in Moment.js

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -37,4 +37,36 @@ $(document).ready(function(){
 
     })
 
+    database.ref().on("child_added", function(newChild) {
+        var nextTrain;
+        var minAway;
+
+        var firstTrainNew = moment(newChild.val().firstTrain, "hh:mm").subtract(1, "years");
+        var differenceInTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = differenceInTime % newChild.val().frequency;
+
+        minAway = newChild.val().frequency - remainder;
+        nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("hh:mm");
+
+        $("#add-row").append("<tr><td>" + newChild.val().name + 
+                            "</td><td>" + newChild.val().destination +
+                            "</td><td>" + newChild.val().frequency +
+                            "</td><td>" + nextTrain +
+                            "</td><td>" + minAway + "</td></tr>");
+
+         }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        }
+    );
+
+    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").html(snapshot.val().name);
+        $("#email-display").html(snapshot.val().email);
+        $("#age-display").html(snapshot.val().age);
+        $("#comment-display").html(snapshot.val().comment);
+    });
+
+
 })
