af13a5706fadfa4bacdd4b307fe53556ea7558cd
Eliska Patockova <patockovaeliska@gmail.com>
2020-03-25 10:02:01 -0700

    set up on click function for submit button

--- assets/javascript/Moment.js
+++ assets/javascript/Moment.js
@@ -17,4 +17,24 @@ $(document).ready(function(){
     var firstTrain;
     var frequency = 0;
 
+    $("#add-train").on("click", function(){
+        event.preventDefault();
+
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        $("form")[0].reset();
+
+    })
+
 })
